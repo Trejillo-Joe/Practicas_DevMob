ddfc97b6be2bc351032d92b18d70ec9bd42c4665
Trejillo-Joe <a20491190@itmexicali.edu.mx>
2025-11-04 21:16:05 -0800

    Cambio 04/11/2025

--- flutter_cinecito/lib/presentation/screen/home_screen.dart
+++ flutter_cinecito/lib/presentation/screen/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String routeName = 'home_screen';
@@ -6,8 +7,13 @@ class HomeScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
+      print(
+         dotenv.env['TOKEN_SECRET_TMDB']);
+      print( 
+        dotenv.env['API_TMDB']);
+      
+      return const Scaffold(
+        body: Center(
         child: Text("Welcome to Cinecito Home Screen"),
       ),
     );
